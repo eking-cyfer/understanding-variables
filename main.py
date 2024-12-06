6e64fb0774da3a5c9bab90bb2fc98cb404304e85
eking-cyfer <edwardafatsawo20@gmail.com>
2024-12-06 20:00:36 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#creating different variables
+x = 5
+y = "Nick is Ewdwards friend"
+print(x)
+print(y)
+
+codingal = "i am coding using codingal"
+print(codingal)
+
+# user input
+name = input("my name is Edward")
+
+print("\n Hello", name, "\n Welcome to Codingal")
